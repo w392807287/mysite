a64f20a97a9d43046f01a7c15583a7f5e54f713a
w392807287 <392807287@qq.com>
2016-12-28 22:35:45 +0800

    改用mysql‘

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from mysite import private_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,8 +89,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': private_settings.DATABASE_NAME,
+        'USER': private_settings.DATABASE_USERNAME,
+        'PASSWORD': private_settings.DATABASE_PASSWORD,
+        'HOST': private_settings.DATABASE_HOST,
+        'PORT': private_settings.DATABASE_PORT,
     }
 }
 
